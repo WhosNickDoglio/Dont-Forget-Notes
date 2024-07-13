2aab5ff05d8458f71f0c2ad50f4460da7f5ea9aa
nicholasdoglio <NicholasDoglio@Gmail.com>
2024-07-13 14:39:27 -0400

    Fix test

--- build-logic/src/main/java/dev/whosnickdoglio/baenotes/convention/configurations/AndroidLibraryConfiguration.kt
+++ build-logic/src/main/java/dev/whosnickdoglio/baenotes/convention/configurations/AndroidLibraryConfiguration.kt
@@ -39,15 +39,6 @@ internal class AndroidLibraryConfiguration : Configuration {
                     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
                     consumerProguardFiles("consumer-rules.pro")
                 }
-
-                buildTypes {
-                    release {
-                        isMinifyEnabled = true
-                        proguardFiles(
-                            getDefaultProguardFile("proguard-android-optimize.txt"),
-                            "proguard-rules.pro")
-                    }
-                }
                 compileOptions {
                     sourceCompatibility = NotesConfiguration.javaVersion
                     targetCompatibility = NotesConfiguration.javaVersion
