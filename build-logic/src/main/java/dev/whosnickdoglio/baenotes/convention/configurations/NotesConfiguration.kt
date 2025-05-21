db39610d6ef32754e01ac90733492d7e8d5ade73
nicholasdoglio <NicholasDoglio@Gmail.com>
2025-05-21 08:14:55 -0400

    Target/Compile SDK 36

--- build-logic/src/main/java/dev/whosnickdoglio/baenotes/convention/configurations/NotesConfiguration.kt
+++ build-logic/src/main/java/dev/whosnickdoglio/baenotes/convention/configurations/NotesConfiguration.kt
@@ -30,6 +30,6 @@ import org.gradle.api.JavaVersion
 public object NotesConfiguration {
     public val javaVersion: JavaVersion = JavaVersion.VERSION_17
     public const val MIN_SDK: Int = 30
-    public const val TARGET_SDK: Int = 35
-    public const val COMPILE_SDK: Int = 35
+    public const val TARGET_SDK: Int = 36
+    public const val COMPILE_SDK: Int = 36
 }
