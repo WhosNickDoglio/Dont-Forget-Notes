4d6684ca2a23b6f2d15a9654b7a5e93c6eaf1526
nicholasdoglio <NicholasDoglio@Gmail.com>
2025-08-17 19:24:15 -0400

    WIP Glance 1.2

--- widget/src/main/java/dev/whosnickdoglio/baenotes/widget/BaeNoteWidget.kt
+++ widget/src/main/java/dev/whosnickdoglio/baenotes/widget/BaeNoteWidget.kt
@@ -26,19 +26,32 @@
 package dev.whosnickdoglio.baenotes.widget
 
 import android.content.Context
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.runtime.Composable
 import androidx.glance.GlanceId
 import androidx.glance.GlanceTheme
 import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.GlanceAppWidgetManager
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
 import androidx.glance.appwidget.SizeMode
+import androidx.glance.appwidget.composeForPreview
 import androidx.glance.appwidget.provideContent
+import androidx.glance.appwidget.setWidgetPreviews
 import androidx.glance.currentState
+import androidx.glance.preview.ExperimentalGlancePreviewApi
+import androidx.glance.preview.Preview
 import androidx.glance.state.GlanceStateDefinition
+import dev.whosnickdoglio.baenotes.model.NoteColor
 import dev.whosnickdoglio.baenotes.model.NoteWidgetState
 import dev.whosnickdoglio.baenotes.widget.internal.BaeNoteWidgetStateDefinition
 import dev.whosnickdoglio.baenotes.widget.internal.NoteWidget
 
 public object BaeNoteWidget : GlanceAppWidget() {
+
+    public class Receiver : GlanceAppWidgetReceiver() {
+        override val glanceAppWidget: GlanceAppWidget = BaeNoteWidget
+    }
 
     override val stateDefinition: GlanceStateDefinition<*> = BaeNoteWidgetStateDefinition
 
@@ -52,8 +65,35 @@ public object BaeNoteWidget : GlanceAppWidget() {
             }
         }
     }
+
+    override suspend fun providePreview(
+        context: Context,
+        widgetCategory: Int
+    ) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.VANILLA_ICE_CREAM) {
+            GlanceAppWidgetManager(context).setWidgetPreviews(Receiver::class)
+        }
+        provideContent { NoteWidgetPreview() }
+    }
 }
 
-public class BaeNoteWidgetReceiver : GlanceAppWidgetReceiver() {
-    override val glanceAppWidget: GlanceAppWidget = BaeNoteWidget
+@Composable
+private fun NoteWidgetPreview() {
+    GlanceTheme {
+        NoteWidget(
+            state = NoteWidgetState(
+                content = "Make Vet appointment for Wheat Thin",
+                textColor = NoteColor.WHITE,
+                backgroundColor = NoteColor.PINK
+            )
+        )
+    }
 }
+
+//@OptIn(ExperimentalGlancePreviewApi::class)
+//@Preview
+//@Composable
+//private fun NoteWidgetPreviewPreview() {
+//    BaeNoteWidget.composeForPreview()
+//
+//}
