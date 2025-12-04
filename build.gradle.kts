47016c05ca3f85de02136579840ce473c5fcf937
nicholasdoglio <NicholasDoglio@Gmail.com>
2025-12-04 08:09:13 -0500

    WIP Switch to convention plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,30 +30,26 @@ import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 
 plugins {
     alias(libs.plugins.dependency.analysis)
-//    alias(libs.plugins.dependencyGuard)
+    alias(libs.plugins.dependencyGuard) apply false
     alias(libs.plugins.doctor)
     alias(libs.plugins.gradle.versions)
     alias(libs.plugins.android.app) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.android.lint) apply false
+    alias(libs.plugins.convention.app) apply false
+    alias(libs.plugins.convention.android.library) apply false
+    alias(libs.plugins.convention.jvm) apply false
+    alias(libs.plugins.convention.kmp) apply false
     alias(libs.plugins.cacheFix) apply false
     alias(libs.plugins.composeGuard) apply false
     alias(libs.plugins.detekt) apply false
+    alias(libs.plugins.ktfmt) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.licensee) apply false
-    alias(libs.plugins.spotless) apply false
     alias(libs.plugins.sortDependencies) apply false
-}
-
-//dependencyGuard { configuration("classpath") }
-
-doctor {
-    @Suppress("DEPRECATION")
-    warnWhenNotUsingParallelGC = false
-    javaHome { failOnError = false }
 }
 
 // https://docs.gradle.org/8.9/userguide/gradle_daemon.html#daemon_jvm_criteria
